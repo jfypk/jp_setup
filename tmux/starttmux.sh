4c2c82baca35d690afd4caa0f148dd1403944884
Jeff Park <jypark03@gmail.com>
2022-08-20 00:24:41 -0400

    deleting taskwarrior

--- tmux/starttmux.sh
+++ tmux/starttmux.sh
@@ -5,21 +5,23 @@ SESSIONEXISTS=$(tmux list-sessions | grep $session)
 if [ "$SESSIONEXISTS" = "" ]
 then
   # create a new tmux session, starting a new vim session
-  tmux new-session -d -s $session -n vim
+  tmux new-session -d -s $session -n editor
   tmux send-keys "v" C-m
+  tmux splitw -v -p 15
+  tmux splitw -h -p 50
 
   tmux new-window -n 'git & tasks'
-  tmux send-keys "cd ~/Documents/Code" C-m
+  # tmux send-keys "cd ~/Documents/Code" C-m
   tmux splitw -h -p 50
    # Select pane 2
-  tmux selectp -t 2
-  tmux send-keys "~/.jp_setup/taskwarrior/taskwarrior-tui" C-m \;
-  tmux selectp -t 1
+  # tmux selectp -t 2
+  # tmux send-keys "~/.jp_setup/taskwarrior/taskwarrior-tui" C-m \;
+  # tmux selectp -t 1
 
   tmux new-window -n 'shell'
 
   # return to main vim window
-  tmux select-window -t 'vim'
+  tmux select-window -t 'editor'
 
   # Finished setup, attach to the tmux session!
   tmux attach-session -t $session
--- vimrc
+++ vimrc
@@ -240,7 +240,11 @@ let NERDTreeAutoDeleteBuffer = 1
 let NERDTreeShowHidden=1
 
 " Start NERDTree and put the cursor back in the other window.
-autocmd VimEnter * NERDTree | wincmd p
+" autocmd VimEnter * NERDTree | wincmd p
+
+" Start NERDTree. If a file is specified, move the cursor to its window.
+" autocmd StdinReadPre * let s:std_in=1
+" autocmd VimEnter * NERDTree | if argc() > 0 || exists("s:std_in") | wincmd p | endif
 
 " If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
 " autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
